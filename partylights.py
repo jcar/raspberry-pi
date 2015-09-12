e1359dbce6939ad0e0646f4ce2dcfee5371c7aa6
Jason Carter <jason.joe.carter@gmail.com>
2015-09-12 13:57:43 -0500

    s

--- partylights.py
+++ partylights.py
@@ -11,7 +11,7 @@ flash_delay = .10
 try:
     while True:
         input_state = GPIO.input(21)
-        if input_state == False:
+        if input_state == True:
             GPIO.output(17,GPIO.HIGH)
             GPIO.output(22,GPIO.LOW)
             time.sleep(flash_delay)
